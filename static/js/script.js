d6394f78cdc16844ffaa73702b7f5f241d531a92
Balogh István Barnabás <bdbarni@gmail.com>
2024-02-29 21:52:48 +0100

    implementing prefetch

--- static/js/script.js
+++ static/js/script.js
@@ -1,205 +1,209 @@
-//Base state
-//Api fetch
-fetch('/media/all')
-.then(response => {
-if (!response.ok) {
-    throw new Error('Hálózati válasz nem volt oké');
+//Pre fetch
+
+//Expired media list prefetch
+let ExpiredMediaJson = null;
+
+async function getExpiredMedia(){
+    try {
+        const response = await fetch('/media/expired');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        ExpiredMediaJson = data;
+    } catch (error) {
+        console.error('Hiba történt az adatok lekérése közben:', error);
+    }
 }
-return response.json();
-})
-.then(data => {
-    listFiles(data);
-})
-.catch(error => {
-    console.error('Hiba történt az API lekérése során:', error);
-});
 
-    //List files for delete
-    var selected_media = new Set([]);
-    let all_media_id = new Set([]);
-    const typemap = new Map();
+getExpiredMedia();
 
-    //List files
-    function listFiles(data) {
 
-        //Set to base state
-        selected_media.clear();
-        all_media_id.clear();
-        typemap.clear();
+//All media list prefetch
+let AllMediaJson = null;
 
-        const list = document.querySelector('section');
-        removeChildren(list);
+async function getAllMedia(){
+    try {
+        const response = await fetch('/media/all');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        AllMediaJson = data;
+    } catch (error) {
+        console.error('Hiba történt az adatok lekérése közben:', error);
+    }
+}
 
-        data.sort((a, b) => a[0].localeCompare(b[0]));
+//getAllMedia();
+
+//Global variables
+let selected_media = new Set([]);
+let all_media_id = new Set([]);
+const typemap = new Map();
+
+//Base state
+
+async function setupBaseState(){
+    await getAllMedia();
+    listFiles(AllMediaJson);
+}
+
+setupBaseState();
+
+//List files
+async function listFiles(data) {
+
+    //Set to base state
+    selected_media.clear();
+    all_media_id.clear();
+    typemap.clear();
+
+    const list = document.querySelector('section');
+    removeChildren(list);
+
+    data.sort((a, b) => a[0].localeCompare(b[0]));
+    
+    for(let str of data){
         
-        for(let str of data){
-            
-            all_media_id.add(str[2]);
-            typemap.set(str[2], [str[0], str[3]]);
-            let list_item_container = document.createElement("div");
-            list_item_container.className = "movie-list-item";
-            let list_item_input = document.createElement("input");
-            list_item_input.type = "checkbox";
-            list_item_input.addEventListener('change', function(){
-                if(this.checked){
-                    selected_media.add(str[2]);
-                }else{
-                    selected_media.delete(str[2]);
-                }
-            });
-            let list_text_container = document.createElement("div");
-            list_text_container.className = "list-text-container";
-            let list_item_label = document.createElement("label");
-            list_item_label.className = "title";
-            list_item_label.textContent = str[0];
-            let list_item_watchtime = document.createElement("label");
-            list_item_watchtime.id = "watchtime";
-            list_item_watchtime.textContent = "Last: "+str[1]+ " days ago";
-            list_text_container.appendChild(list_item_label);
-            list_text_container.appendChild(list_item_watchtime);
+        all_media_id.add(str[2]);
+        typemap.set(str[2], [str[0], str[3]]);
+        let list_item_container = document.createElement("div");
+        list_item_container.className = "movie-list-item";
+        let list_item_input = document.createElement("input");
+        list_item_input.type = "checkbox";
+        list_item_input.addEventListener('change', function(){
+            if(this.checked){
+                selected_media.add(str[2]);
+            }else{
+                selected_media.delete(str[2]);
+            }
+        });
+        let list_text_container = document.createElement("div");
+        list_text_container.className = "list-text-container";
+        let list_item_label = document.createElement("label");
+        list_item_label.className = "title";
+        list_item_label.textContent = str[0];
+        let list_item_watchtime = document.createElement("label");
+        list_item_watchtime.id = "watchtime";
+        list_item_watchtime.textContent = "Last: "+str[1]+ " days ago";
+        list_text_container.appendChild(list_item_label);
+        list_text_container.appendChild(list_item_watchtime);
 
-            list_item_container.appendChild(list_item_input);
-            list_item_container.appendChild(list_text_container);
-            list.appendChild(list_item_container);
-        }
+        list_item_container.appendChild(list_item_input);
+        list_item_container.appendChild(list_text_container);
+        list.appendChild(list_item_container);
     }
+}
 
-    //Remove all children from parent
-    function removeChildren(parent){
-        while(parent.firstChild){
-            parent.removeChild(parent.firstChild);
-        }
+//Remove all children from parent
+function removeChildren(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
     }
+}
 
-    //Set empty list state
-    function emptyListHandler(text){
-        let base_text = document.createElement("span");
-        base_text.textContent = text;
-        base_text.className = "base-text";
-        document.querySelector('section').appendChild(base_text);
+//Set empty list state
+function emptyListHandler(text){
+    let base_text = document.createElement("span");
+    base_text.textContent = text;
+    base_text.className = "base-text";
+    document.querySelector('section').appendChild(base_text);
+}
+
+//Select all button setup
+const sel_all = document.querySelector('#sel_all');
+sel_all.addEventListener('click', function(){
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for(let checkbox of checkboxes){
+        checkbox.checked = true;
+    
     }
-
-    //Select all button setup
-    const sel_all = document.querySelector('#sel_all');
-    sel_all.addEventListener('click', function(){
-        let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        for(let checkbox of checkboxes){
-            checkbox.checked = true;
-        
-        }
-        for(let id of all_media_id){
-            selected_media.add(id);
-        }
-    });
-
-    //Select none button setup
-    const sel_none = document.querySelector('#sel_none');
-    sel_none.addEventListener('click', function(){
-        let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        for(let checkbox of checkboxes){
-            checkbox.checked = false;
-        }
-        selected_media.clear();
-    });
-
-    //List clear button setup
-    const list_clear = document.querySelector('#clear');
-    list_clear.addEventListener('click', function(){
-        //Delete section content
-        removeChildren(document.querySelector('section'));
-        emptyListHandler("No files to show");
-    });
-
-    //List all button setup
-    const list_all = document.querySelector('#list_all');
-    list_all.addEventListener('click', function(){
-
-    //Api fetch
-    fetch('/media/all')
-    .then(response => {
-    if (!response.ok) {
-        throw new Error('Hálózati válasz nem volt oké');
+    for(let id of all_media_id){
+        selected_media.add(id);
     }
-    return response.json();
-    })
-    .then(data => {
-        listFiles(data);
-    })
-    .catch(error => {
-        console.error('Hiba történt az API lekérése során:', error);
-    });
 });
 
-    //List expired button setup
-    const list_expired = document.querySelector('#list_expired');
-    list_expired.addEventListener('click', function(){
-
-    //Api fetch
-    fetch('/media/expired')
-    .then(response => {
-    if (!response.ok) {
-        throw new Error('Hálózati válasz nem volt oké');
+//Select none button setup
+const sel_none = document.querySelector('#sel_none');
+sel_none.addEventListener('click', function(){
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for(let checkbox of checkboxes){
+        checkbox.checked = false;
     }
-    return response.json();
-    })
-    .then(data => {
-        listFiles(data);
-    })
-    .catch(error => {
-        console.error('Hiba történt az API lekérése során:', error);
-    });
+    selected_media.clear();
 });
 
-    //Delete button setup
-    const button = document.querySelector('.delete-button');
-    button.addEventListener('click', function(){
-        //Make JSON for API
-        let jsonforsend = [];
-        for(let id of selected_media){
-            jsonforsend.push([id, typemap.get(id)[1]]);
-        }
+//List clear button setup
+const list_clear = document.querySelector('#clear');
+list_clear.addEventListener('click', function(){
+    //Delete section content
+    removeChildren(document.querySelector('section'));
+    emptyListHandler("No files to show");
+});
 
-    //Api fetch
-    fetch('/media',{
-        method:'POST',
-        body: JSON.stringify(Array.from(jsonforsend)),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    .then(response => {
-        return response.json();
-    })
-    .then(data =>{
-        console.log(data);
-    })
+//List all button setup
+const list_all = document.querySelector('#list_all');
+list_all.addEventListener('click', function(){
+    listFiles(AllMediaJson);
+});
 
-        //Delete section content
-        removeChildren(document.querySelector('section'));
-        emptyListHandler("Deleted");
-    });
+//List expired button setup
+const list_expired = document.querySelector('#list_expired');
+list_expired.addEventListener('click', function(){
+    listFiles(ExpiredMediaJson);
+});
+
+//Delete button setup
+const button = document.querySelector('.delete-button');
+button.addEventListener('click', function(){
+    //Make JSON for API
+    let jsonforsend = [];
+    for(let id of selected_media){
+        jsonforsend.push([id, typemap.get(id)[1]]);
+    }
+
+//Api fetch
+fetch('/media',{
+    method:'POST',
+    body: JSON.stringify(Array.from(jsonforsend)),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+.then(response => {
+    return response.json();
+})
+.then(data =>{
+    console.log(data);
+})
+
+    //Delete section content
+    removeChildren(document.querySelector('section'));
+    emptyListHandler("Deleted");
+});
 
 
-    //Settings button setup
-    var popup = document.querySelector('#SettingsModal');
+//Settings button setup
+var popup = document.querySelector('#SettingsModal');
 
-    var btn = document.querySelector('.settings');
+var btn = document.querySelector('.settings');
 
-    var span = document.querySelector('.close');
+var span = document.querySelector('.close');
 
-    btn.addEventListener('click', function(){   
-        popup.style.display = 'block';
-    });
+btn.addEventListener('click', function(){   
+    popup.style.display = 'block';
+});
 
-    span.addEventListener('click', function(){
+span.addEventListener('click', function(){
+    popup.style.display = 'none';
+});
+
+window.addEventListener('click', function(event){
+    if (event.target == popup) {
         popup.style.display = 'none';
-    });
-
-    window.addEventListener('click', function(event){
-        if (event.target == popup) {
-            popup.style.display = 'none';
-        }
-    });
+    }
+});
 
 //Collapsible sections setup
 var collapsible = document.getElementsByClassName("collapsible");
